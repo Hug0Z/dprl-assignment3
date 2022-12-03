2e6bc3915a78218e95d2d04670bfd450865e8f48
H.zwaan <hugo.zwaan.zwaan@hva.nl>
2022-12-03 18:49:47 +0100

    WIP

--- assignment3.py
+++ assignment3.py
@@ -11,20 +11,56 @@ COLUMNS = 3
 DIAGONALS = 2
 
 
-class Node:
+class Node(object):
     def __init__(self, board: np.array, moves: list) -> None:
         self.board = board
         self.moves = moves
 
 
+class Tree(object):
+    def __init__(self, data: int = -1, count: int = 0, reward: int = 0):
+        self.move = data
+        self.count = count
+        self.reward = reward
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+    # deze mag nog wel verbeterd worden zodat je de hele tree kan
+    # printen ik heb nu beetje geprobeerd maar het lukte niet echt.
+    def print_children(self):
+        for c in self.children:
+            print(
+                "{} reward: {:>{width}}\t count: {:>{width}}".format(
+                    c.move, c.reward, c.count, width=8
+                )
+            )
+        print("printed children of: ", self.move)
+
+    def find_child(self, move):
+        for c in self.children:
+            if c.move == move:
+                return 1
+        return 0
+
+    def walk_tree_add_reward(self, moves, reward):
+        for move in moves:
+            for c in self.children:
+                if c.move == move:
+                    c.count += 1
+                    c.reward += reward
+                    self = c
+
+
 class TicTacToe:
+    converter = lambda _: {EMPTY: " ", CROSSES: "X", CIRCLES: "O"}[_]
+
     def __init__(self) -> None:
         self.board = np.array(
             (EMPTY, EMPTY, EMPTY, EMPTY, CROSSES, EMPTY, EMPTY, EMPTY, EMPTY)
         )
-        self.converter = lambda _: {EMPTY: " ", CROSSES: "X", CIRCLES: "O"}[_]
-        """Create a matrix/ vector to save all possible Q values to create a convergent tree"""
-        """Create a matrix/ vector to save the attempts in this possible node"""
+        self.player_made_moves = []
 
     def winning(self, board: np.array) -> tuple[bool, int]:
         """Returns the winner of a game
@@ -109,20 +145,15 @@ class TicTacToe:
         """
         possible_moves = self.possible_moves(board)
         if len(possible_moves) != 0:
-            board[random.choice(possible_moves)] = CROSSES
+            move = random.choice(possible_moves)
+            board[move] = CROSSES
             return board
         else:
             raise KeyError("Board is full")
 
-    def selection(self, board: np.array) -> list:
-        node = [0]
-        if random.choice([True, False]):
-            return node
-        else:
-            move = random.choice(self.possible_moves(board))
-            node.append(move)
-
-    def get_Q_values(self, node: Node = None, recursive: bool = True) -> list:
+    def get_Q_values(
+        self, current_node, node: Node = None, recursive: bool = True
+    ) -> list:
         """calculates the optimal strategy for play tic tac toe
 
         Args:
@@ -134,33 +165,63 @@ class TicTacToe:
         """
         # SLIDE 28 lecture 7
         termination, winner = self.winning(node.board)
-        draw = len(self.possible_moves(node.board)) == 0
-        if termination or draw:
+        if termination:
             return 1 if winner == CIRCLES else 0
+        elif len(self.possible_moves(node.board)) == 0:
+            return 0.5
 
-        new_board = self.opponent_move(node.board) if recursive else self.board
+        new_board = self.opponent_move(node.board) if recursive else self.board.copy()
+
         moves = self.possible_moves(new_board)
         if len(moves) > 0:
-            new_board[random.choice(moves)] = CIRCLES
-        node_prime = Node(new_board, self.possible_moves(new_board))
+            selected_node = random.choice(moves)
+            # add newly found node. first check if it is already part of the children
+            # check if the move is in the tree in the current state
+            # if it is not in the tree add a new child node.
+            res = current_node.find_child(selected_node)
+            if res == 0:
+                current_node.add_child(Tree(selected_node, 0))
 
-        print(self.print_board(new_board))
-        return self.get_Q_values(node_prime)
+            self.player_made_moves.append(selected_node)
+            # laat de current_node wijzen naar de juiste node dus de child die net is toegevoegd.
+            for c in current_node.children:
+                if c.move == selected_node:
+                    current_node = c
+            new_board[selected_node] = CIRCLES
+        node_prime = Node(new_board.copy(), self.possible_moves(new_board))
 
-    def selection(self) -> None:
-        """randomly select point to do expantion and rollout on"""
-        pass
+        # print(self.print_board(new_board))
+        return self.get_Q_values(current_node, node_prime)
 
-    def expantion(self) -> list:
-        """return a list of all childeren of the found selection node"""
-        pass
+    def Q_convergence(self, epsilon=0.001) -> None:
+        """Simulates the Game until convergence is achieved
 
-    def rollout(self) -> dict:
-        """returns all ternimation node rewards, by playing all expantion childeren until they terminate"""
-        """also update the Q value of the selection node (for convergence)"""
-        pass
-        
+        Args:
+            epsilon (float, optional): max difference between iterations. Defaults to 0.001.
+        """
+        tree = Tree()
+        current_node = tree
+
+        for _ in range(5000):
+            self.__init__()
+            # met de reward kunnen we een counter toevoegen aan hoe vaak een bepaalde node in de tree gewonnen heeft.
+            # hiervoor moeten we een tree walker maken zodat we de juiste stappen kunnen nemen om bij de juiste nodes de counter te incrementen.
+            reward = self.get_Q_values(
+                current_node, node=Node(ttt.board, []), recursive=False
+            )
+            # nu zetten we current_node weer terug naar de start node zodat de eerste stap die gemaakt wordt wordt toegevoegd aan de eerste node.
+            current_node = tree
+            # if reward == 1:
+            #     print(reward)
+            # print(player_made_moves)
+            tree.walk_tree_add_reward(self.player_made_moves, reward)
+
+        # print alle nodes in de tree nog niet heel mooi als je helemaal naar boven scrolled zie je de eerste node die heeft 8 children dit zijn de 8 eerste moves.
+        # heb voor nu uitgezet dat alle nodes worden geprint alleen de eerste node met ze children word geprint en dat zijn alle eerste moves die gemaakt zijn.
+        # als je het weer aan wilt zetten kan je dat doen in de functie print_children door de commented lines te uncommenten.
+        current_node.print_children()
+
 
 if __name__ == "__main__":
     ttt = TicTacToe()
-    print(ttt.get_Q_values(node=Node(ttt.board, []), recursive=False))
+    ttt.Q_convergence()
